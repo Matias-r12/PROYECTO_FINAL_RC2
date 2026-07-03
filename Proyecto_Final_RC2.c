ce4f6ef33cd587b984921cdc912da9c54132236d
Matias.Rojas <matias.rojas.suntaxi@udla.edu.ec>
2026-07-03 15:11:15 -0500

    Agregamos las funciones que vamos a utilizar

--- Proyecto_Final_RC2.c
+++ Proyecto_Final_RC2.c
@@ -1,3 +1,6 @@
+//Nombre de los integrantes: Matias Rojas
+//Fecha 29/6/2026
+//Descripcion:Caso 4,Citas medicas
 #include <stdio.h>
 #include <string.h>
 typedef struct{
@@ -11,3 +14,12 @@ int HoraH;
 int HoraM;
 char Doc[16];
 }citas;
+void escribircita(citas* e, citas* arr, int pos);
+void buscarCita(citas *a, int tam);
+int fechaValida(int dia, int mes, int anio);
+int horaValida(int hora, int minuto);
+int fechaHoraRepetida(citas *arr, int n, citas *nueva);
+void mostrarcita(citas*a,int tam);
+void guardararchivo(citas*a,int tam);
+
+
