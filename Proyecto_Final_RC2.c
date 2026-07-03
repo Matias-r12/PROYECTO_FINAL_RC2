21c7b86438801d1855fb2033c90b40724529ab3e
Matias.Rojas <matias.rojas.suntaxi@udla.edu.ec>
2026-07-03 15:03:58 -0500

    Primero definimos la estructura citas para empezar

---
+++ Proyecto_Final_RC2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+typedef struct{
+int Cit;
+char Paciente[16];
+char Especialidad[16];
+int FechaD;
+int FechaM;
+int FechaA;
+int HoraH;
+int HoraM;
+char Doc[16];
+}citas;
